c3c19191fab0824130a57de91ba06cd82327b6ac
Michelangelo Barulli <michelangelobaru@gmail.com>
2020-03-17 10:33:39 +0100

    part 1

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,66 +1,39 @@
 package it.polito.tdp.libretto.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * Memorizza e gestisce un insieme di voti superati.
- * 
- * @author Fulvio
+ * Memorizza e gestisce un insieme di voti superati
+ * @author USER
  *
  */
 public class Libretto {
-
+	
 	private List<Voto> voti = new ArrayList<>();
-
-	/**
-	 * Aggiunge un nuovo voto al libretto
-	 * 
-	 * @param v Voto da aggiungere
-	 */
-	public void add(Voto v) {
+	
+	public void add(Voto v) { //operazione di "delega"
 		this.voti.add(v);
-	}
-
-	/**
-	 * Dato un Libretto, restituisce una stringa nella quale vi sono solamente i
-	 * voti pari al valore specificato
-	 * 
-	 * @param voto valore specificato
-	 * @return stringa formattata per visualizzare il sotto-libretto
-	 */
-	public String stampaVotiUguali(int voto) {
-		String s = "";
-		for (Voto v : this.voti) {
-			if (v.getVoto() == voto) {
-				s += v.toString() + "\n";
-			}
-		}
-		return s;
 	}
 	
 	/**
-	 * Genera un nuovo libretto, a partire da quello esistente,
-	 * che conterrà esclusivamenti i voti con votazione pari a quella specificata.
-	 * @param voto votazione specificata
+	 * genera un nuovo libretto, a partire da quello esistente,
+	 * che conterra esclusivamente i voti con votaz pari a quella specificata
+	 * @param voto con votazione specificata
 	 * @return nuovo Libretto "ridotto"
 	 */
 	public Libretto estraiVotiUguali(int voto) {
-		Libretto nuovo = new Libretto() ;
+		Libretto nuovo = new Libretto();
 		for(Voto v: this.voti) {
-			if(v.getVoto()==voto) {
+			if(v.getVoto()==voto)
 				nuovo.add(v);
-			}
 		}
-		return nuovo ;
+		return nuovo;
 	}
-
 	public String toString() {
-		String s = "";
-		for (Voto v : this.voti) {
-			s += v.toString() + "\n";
+		String s ="";
+		for(Voto v:this.voti) {
+			s+= v.toString()+"\n";
 		}
 		return s;
 	}
-
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -4,27 +4,21 @@ import java.time.LocalDate;
 
 public class TestLibretto {
 	
-	Libretto lib ;
-	
-	private void run() {
-		this.lib = new Libretto() ; // crea libretto vuoto
+	Libretto lib;
+	private void run() { //uso questo perché non è static! non si lavora dentro il main, si crea un altro metodo richiamato dal main
+		this.lib = new Libretto(); //crea libretto vuoto
+		Voto v1 = new Voto("Tecniche di programmazione", 30, LocalDate.of(2020, 06, 15));
+		Voto v2 = new Voto("Analisi 2", 28, LocalDate.of(2020, 06, 28));
 		
-		Voto v1 = new Voto("Tecniche di programmazione", 30, LocalDate.of(2020, 06, 15)) ;
-		Voto v2 = new Voto("Analisi II", 28, LocalDate.of(2020, 06, 28)) ;
-
 		lib.add(v1);
 		lib.add(v2);
 		lib.add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14)));
 		
-		System.out.println(this.lib) ;
-		
-		System.out.println(this.lib.stampaVotiUguali(28)) ;
-		
-		System.out.println(this.lib.estraiVotiUguali(28)) ;
+		System.out.println(this.lib);
+		System.out.println(this.lib.estraiVotiUguali(28).toString());
 	}
-
 	public static void main(String[] args) {
-		TestLibretto test = new TestLibretto() ;
+		TestLibretto test = new TestLibretto();
 		test.run();
 	}
 
